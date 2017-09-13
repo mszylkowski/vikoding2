0b6c4e4d25aeede835b7cbd37416b8e02688510d
Matias Szylkowski <mszylkowski@hotmail.com>
2017-09-13 14:37:33 -0400

    did not push file before, uplodaing p3

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,6 +43,10 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+      String output = "";
+      for (int i = 0; i < input.length(); i++) {
+        output = input.charAt(i) + output;
+      }
+	  return output;
 	}
 }
